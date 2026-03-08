5280f2d5620588600d2b76b3d9022ac45e29c587
Sdmist <siddash_always@outlook.com>
2026-03-09 02:00:41 +0530

    Refactor counting methods for occurrences of '1'
    Refactored the code to include three methods for counting occurrences of '1': using map, list, and string approaches.

--- solutions/Team.py
+++ solutions/Team.py
@@ -1,12 +1,32 @@
-import sys
-
-input = sys.stdin.readline
+#Method-1: Map Approach 
 
 n = int(input())
-count =  0
-for p in range(n):
-    s = input().strip()
-    if s.count("1") >= 2:
-        count += 1
-print(count)
+ans = 0
+for _ in range(n):
+    A = map(int, input().split())
+    cnt1 = 0
+    for x in A: 
+        if x == 1: cnt1 += 1
+    if cnt1 >= 2:
+        ans += 1
+print(ans)
 
+#Method-2: List Approach
+
+n = int(input())
+ans = 0
+for _ in range(n): 
+    list_ = list(map(int, input().split()))
+    if list_.count(1) >= 2:
+        ans += 1
+print(ans)
+
+#Method-3: String Approach
+
+n = int(input())
+ans = 0
+for _ in range(n):
+    s = input()
+    if s.count('1') >= 2:
+        ans += 1
+print(ans)
