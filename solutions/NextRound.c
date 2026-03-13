798949f964f8d1699f307470469ed6f8702aabd1
Sdmist <siddash_always@outlook.com>
2026-03-14 03:25:07 +0530

    Add files via upload

---
+++ solutions/NextRound.c
@@ -0,0 +1,21 @@
+#include <stdio.h> 
+
+int main() {
+    int n, k;
+    scanf("%d %d", &n, &k);
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    int cnt = 0;
+    for (int i = 0; i < n; i++) {
+        if (a[i] > 0 && a[i] >= a[k-1]) {
+            cnt++;
+        }
+        else {
+            break;
+        }
+    }
+    printf("%d \n", cnt);
+    return(0);
+}
