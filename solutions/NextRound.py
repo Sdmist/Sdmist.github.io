cef22464c02e09ede50897c0a30aa87fe4140b37
Sdmist <siddash_always@outlook.com>
2026-03-18 11:10:06 +0530

    Update NextRound.py

--- solutions/NextRound.py
+++ solutions/NextRound.py
@@ -1,47 +1,49 @@
+#===============================================
 #Method-1: Simple Loop
+#===============================================
 
 n, k = map(int, input().split())
 a = list(map(int, input().split()))
 
 req_score = a[k-1]
-count = 0
+cnt = 0
 for x in a:
-    if x >= req_score and x > 0:
-        count += 1
-    else:
-        break
+    if x >= req_score and x > 0: cnt += 1
+    else: break
 print(count)
 
+#=================================================
 #Method-2: Using sum() with generator expression
+#=================================================
 
 n, k = map(int, input().split())
 a = list(map(int, input().split()))
 
 req_score = a[k-1]
 print(sum(1 for x in a if x >= req_score and x > 0))
-#In fact we can just write 1 here, we can just write,
-#sum(for x in a if x >= req_score and x > 0; here Python takes True = 1 & False = 0
+"""
++In fact we can just write 1 here, we can just write,
++sum(for x in a if x >= req_score and x > 0; here Python takes True = 1 & False = 0
++However mark that, this leads to transversal of the whole array
+"""
 
-#However mark that, this leads to transversal of the whole array
-
-#Method-3: No need to count all
+#==================================================
+#Method-3: Count only if 0
+#==================================================
 
 #if req_score != 0, k players pass by default
+def solve(n, k, a):
+    if a[k-1] != 0:
+        return(k)
+    else:
+        return(a.count(0))
+
 n, k = map(int, input().split())
 a = list(map(int, input().split()))
 
-if a[k-1] != 0:
-    ans = k # + extra
-else: 
-    ans = k - a[:k].count(0) # + extra
+ans = solve(n, k, a)
+print(ans)
 
-#extra are those elements that may be equal to a[k-1] even after the k-1 index
-extra = 0
-i = k
-while a[i] == a[k-1]:
-    extra += 1
-    i += 1
-print(ans + extra)
     
 
 
