f309b7c06e351a33a4ae309e9dd6714f6534c604
Sdmist <siddash_always@outlook.com>
2026-04-11 17:59:55 +0530

    Add files via upload

---
+++ solutions/watermelon.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(Sytem.in);
+        
+        int w = sc.nextInt();
+        if (w > 2 && w % 2 == 0) {
+            system.out.println("YES");
+        }
+        else {
+            system.out.println("NO");
+        }
+    }
+}
