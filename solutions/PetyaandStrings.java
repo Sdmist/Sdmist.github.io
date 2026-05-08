725e9fa6b1df7bd491a411ee8a53a8f4c9320691
Sdmist <siddash_always@outlook.com>
2026-05-09 01:38:39 +0530

    Update PetyaandStrings.java

--- solutions/PetyaandStrings.java
+++ solutions/PetyaandStrings.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        String s1 = sc.next().toLowerCase;
-        String s2 = sc.next().tolowerCase;
+        String s1 = sc.next().toLowerCase();
+        String s2 = sc.next().toLowerCase();
         
-        cmp = s1.compareTo(s2);
+        int cmp = s1.compareTo(s2);
         
         int ans = 0;
         if (cmp < 0) ans = -1;
