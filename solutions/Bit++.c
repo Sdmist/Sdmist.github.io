1510050e9291bc8e1ccfea6ee302a3e04f48eaf5
Sdmist <siddash_always@outlook.com>
2026-05-08 22:53:44 +0530

    Refactor output formatting and variable size

--- solutions/Bit++.c
+++ solutions/Bit++.c
@@ -12,9 +12,7 @@ int main() {
         if (s[1] == '+') x++;
         else x--;
     }
-    printf("%d", x);
-    printf("\n");
-    
+    printf("%d \n", x);
     return(0);
 }
 
@@ -27,24 +25,20 @@ int main() {
     int n;
     scanf("%d", &n);
     int x = 0;
-    char s[5];
+    char s[4];
     while (n--) {
         scanf("%s", s);
-        //strcmp gives the differece between ASCII code of first different characters
+        //strcmp gives the differece between ASCII code of the first different characters
         //positive -> first string is lexigrophically bigger
         //negative -> first string is lexigrophically smaller
-        //So it return 0 when the strings are equal
+        //So it returns 0 when the strings are equal
         if (strcmp(s, "X++") == 0 || strcmp(s, "++X") == 0) {
-            printf("YES");
             x++;
         }
         else {
-            printf("NO");
             x--;
         }
     }
-    printf("%d", x);
-    printf("\n");
-    
+    printf("%d \n", x);
     return(0);
 }
