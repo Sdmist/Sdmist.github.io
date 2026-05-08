c7106d77cde8e14d95f513293e4474880f954227
Sdmist <siddash_always@outlook.com>
2026-05-09 01:40:15 +0530

    Fix sync_with_stdio usage and loop logic

--- solutions/PetyaandStrings.cpp
+++ solutions/PetyaandStrings.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    ios :: sync_with_stdio;
+    ios :: sync_with_stdio(false);
     cin.tie(nullptr);
     
     string s1, s2;
@@ -13,8 +13,12 @@ int main() {
         char c1 = tolower(s1[i]);
         char c2 = tolower(s2[i]);
         
-        if (c1 < c2) ans = -1;
-        else if (c1 > c2) ans = 1;
+        if (c1 < c2) {
+            ans = -1;
+            break;
+        else if (c1 > c2) {
+            ans = 1;
+            break;
     }
     cout << ans << "\n";
 }
