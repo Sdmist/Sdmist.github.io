4063c9ccb24591e68120d7e5e1ea040fd946970c
Sdmist <siddash_always@outlook.com>
2026-05-11 23:46:42 +0530

    Add sorting for integer and string inputs

--- solutions/HelpfulMaths.py
+++ solutions/HelpfulMaths.py
@@ -1,3 +1,8 @@
+#Integer list sorting
 list_ = list(map(int, input().split('+')))
 list_.sort()
 print('+'.join(map(str, list_)))
+
+#String sorting
+s = input()
+print('+'.join(x for x in sorted(x) if x != '+'))
