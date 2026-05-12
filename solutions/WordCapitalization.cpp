5e5ff4bbb620dee64ee8864a0b2a04df681c3d39
Sdmist <siddash_always@outlook.com>
2026-05-12 11:37:11 +0530

    Correct syntax and logic in WordCapitalization.cpp
    Fixes syntax errors and capitalization logic in WordCapitalization.

---
+++ solutions/WordCapitalization.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  sting s;
+  cin >> s;
+  cout << toupper(s[0]);
+  for (int i = 1; i < s.size(); i++) {
+    cout << s[i]; 
+  }
+  cout << "\n";
+  return(0):
+}
