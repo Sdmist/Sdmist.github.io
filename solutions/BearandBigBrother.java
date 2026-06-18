17be07d1582dc9fa0397033f885f4f946314b108
Sdmist <siddash_always@outlook.com>
2026-06-18 16:15:53 +0530

    Create BearandBigBrother.java

---
+++ solutions/BearandBigBrother.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         int a = sc.nextInt();
+         int b = sc.nextInt();
+
+         int itr = 0:;
+         while (a <= b) {
+              a = 3*a;
+              b = 2*b;
+              itr++;
+         }
+         System.out.println(itr);
+    }
+}
