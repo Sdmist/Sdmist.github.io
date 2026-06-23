a71c622f45c48d23c2619a87ccb8e7cb2693d80e
Sdmist <siddash_always@outlook.com>
2026-06-24 01:20:49 +0530

    Add files via upload

--- solutions/WordCapitalization.c
+++ solutions/WordCapitalization.c
@@ -1,15 +1,14 @@
-#include <stdio.h> 
-#include <ctype.h> 
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 int main() {
-  char s[1005];
-  scanf("%s", s);
-  printf("%c", toupper(s[0]));
-  for (int i = 1; s[i] != '\0'; i++) {
-    printf("%c", s[i]);
-  }
-  printf("\n");
-  return(0);
+    char s[105];
+    scanf("%s", &s);
+    
+    printf("%c", toupper(s[0]));
+    for (int i = 1; i < strlen(s); i++) {
+        printf("%c", s[i]);
+    }
+    printf("\n");
 }
-
-  
