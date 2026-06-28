83238f35064a437db1d11242ed4cf610cf5b936c
Sdmist <siddash_always@outlook.com>
2026-06-28 23:17:16 +0530

    Update Word.py

--- solutions/Word.py
+++ solutions/Word.py
@@ -1,9 +1,26 @@
+#==========================
+#Method-1: Using .islower()
+#==========================
+
 s = input()
-lower = 0 
-upper = 0
+lower, upper = 0, 0
 
 for x in s:
   if x.islower(): lower += 1
+  else: upper += 1
+
+if lower >= upper: print(s.lower())
+else: print(s.upper())
+
+#===========================
+#Method-2: Using ord() 
+#===========================
+
+s = input()
+lower, upper = 0, 0
+
+for x in s:
+  if 0 <= ord(x) - ord('a') < 26: lower +=1
   else: upper += 1
 
 if lower >= upper: print(s.lower())
