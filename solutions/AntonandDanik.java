dbff73d6ebc8f0de2b6ee89003c255eacdf1df68
Sdmist <siddash_always@outlook.com>
2026-08-04 00:14:17 +0530

    Add Anton and Danik game logic in AntonandDanik.java

---
+++ solutions/AntonandDanik.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main { 
+  public static void main(String[] args) {
+    new Scanner sc = Scanner(System.in);
+    int n = sc.nextInt();
+    String s = sc.next();
+    int a = 0;
+    for (int i = 0; i < n; i++) {
+      if (s.charAt(i) == 'A') a++;
+    }
+    int d = n - a;
+    if (a < d) System.out.println("Anton");
+    else if (a < d) System.out.println("Danik");
+    else System.out.println("Friendship");
+  }
+}
